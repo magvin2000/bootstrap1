8c762d93783e38a55e1490bff0d9fd007f3cf496
magvin2000 <magvin805gmail.com>
2018-02-14 15:11:27 +0100

    ciao

---
+++ ricevimento_dati.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maggioli
+ * Date: 14/02/2018
+ * Time: 09:05
+ */
+echo ("ecco i dati che ci stai inviando:");
+echo "<br>";
+echo $_GET['email'];
+echo "<br>";
+echo $_GET['password'];
